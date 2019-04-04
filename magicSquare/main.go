0763e9da39d4469137841c2ff7b44d9b40692c71
Aracki <aracki.ivan@gmail.com>
2019-04-04 23:01:16 +0200

    Add twoStrings

---
+++ magicSquare/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+}
+
+func minCostForMagicSquare(s [][]int32) int32 {
+
+	var minCost int32
+
+	return minCost
+}
---
+++ twoStrings/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	s1 := "hello"
+	s2 := "world"
+	fmt.Println(twoStrings(s1, s2))
+}
+
+func twoStrings(s1 string, s2 string) string {
+
+	for i := 0; i < len(s1); i++ {
+		for j := 0; j < len(s2); j++ {
+			if string(s1[i]) == string (s2[j]) {
+				return "YES"
+			}
+		}
+	}
+
+	return "NO"
+}
